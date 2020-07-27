c4f7e5507a49632ec62ef894ac6b8f97c9c270a8
Shamil Gumirov <shamilg@opera.com>
2020-07-27 12:22:53 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -1,3 +1,18 @@
-ln -s ./rebuild-db.sh ../poco/rebuild-db.sh
-ln -s ./db.sh ../poco/db.sh
-ln -s ./conf-gen.sh ../poco/conf-gen.sh
+#!/bin/bash
+
+set -e
+
+function newlink {
+    if [ -L ../poco/$1 ]; then
+	rm ../poco/$1
+    fi
+    ln -s `pwd`/$1 ../poco/$1
+}
+echo "Setting up symlinks..."
+newlink rebuild-db.sh
+echo -e "o rebuild-db.sh\t\tOK"
+newlink db.sh
+echo -e "o db.sh\t\t\tOK"
+newlink conf-gen.sh
+echo -e "o conf-gen.sh\t\tOK"
+echo "Setup is done."
